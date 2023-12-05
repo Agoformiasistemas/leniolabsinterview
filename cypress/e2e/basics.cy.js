ea526705d75b56f476c57581131a00f6f5bf8fc7
Agoformia <ago.formia@gmail.com>
2023-12-05 20:34:07 -0300

    organize the repo

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,29 +1,30 @@
 /// Mocha (is a very famous test framework that Cypress used by default)
 
 describe("Login feature page", () => {
-  before(() => {
-    cy.log("Run the server locally");
-  });
+  // before(() => {
+  //   cy.log("Run the server locally");
+  // });
 
-  after(() => {
-    cy.log("kill the server");
-  });
+  // after(() => {
+  //   cy.log("kill the server");
+  // });
 
-  beforeEach(() => {
-    cy.log("navigate to login page");
-  });
+  // beforeEach(() => {
+  //   cy.log("navigate to login page");
+  // });
 
   //hocks
   //before will run 1 time
   //beforeeach will run 10 times
   //after will run 1 time
   //aftereach 10
-  it("should visit the login page correctly", function () {
-    cy.log("Website should have the correct URL");
+  it("Example", () => {
+    cy.visit("https://demo.seleniumeasy.com/");
+    cy.get("div.container").eq(0).click();
   });
 
-  it("should visit the login page correctly", function () {
-    cy.log("fill the username and the password and login");
-    cy.log("should navigate the login page");
-  });
+  // it("should visit the login page correctly", function () {
+  //   cy.log("fill the username and the password and login");
+  //   cy.log("should navigate the login page");
+  // });
 });
